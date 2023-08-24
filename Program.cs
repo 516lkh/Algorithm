4f9ff9f0038811f0fcaee9a6bfac4593a0e2ad72
516lkh <516rudgus@gmail.com>
2023-08-24 10:26:53 +0900

    평균

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class _1_1OddEven
+    {
+        public string solution(int num)
+        {
+            if (num % 2 == 0) return "Even";
+            else return "Odd";
+        }
+    }
+
+    public class _1_2Average
+    {
+        public double solution(int[] arr)
+        {
+            if (arr.Length == 0) { return 0; }
+
+            double average = 0;
+            foreach(int i in arr)
+            {
+                average += (double)i;
+            }
+            if (average == 0) return average;
+            else return average / arr.Length;
+        }
+    }
+}
