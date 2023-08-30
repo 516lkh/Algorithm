008f949894fc1b7b9afb783184e6757dce8b1204
516lkh <516rudgus@gmail.com>
2023-08-30 10:05:41 +0900

    8-30

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,9 @@ namespace Algorithm
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(A_1_11_harshadnumber.solution(11));
+            int[] arr = A_1_19_Remove_smallest_number.solution(new int[] { 1, 2, 3, 4, 5 });
+            foreach (int i in arr) { Console.Write(i); }
+            Console.WriteLine();
         }
     }
 
@@ -160,7 +163,7 @@ namespace Algorithm
             int _x = x;
             int sum = 0;
 
-            for(int i = 0; i < _x.ToString().Length; i++)
+            for (int i = 0; i < _x.ToString().Length; i++)
             {
                 sum += x % 10;
                 x /= 10;
@@ -191,13 +194,110 @@ namespace Algorithm
     {
         public static int solution(int num)
         {
-            for(int i=0; i < 500; i++)
+            for (int i = 0; i < 500; i++)
             {
                 if (num == 1) return i;
                 num = (num % 2 == 0) ? num / 2 : num * 3 + 1;
             }
 
             return -1;
+        }
+    }
+
+    public class A_1_14_Kim
+    {
+        public string solution(string[] seoul)
+        {
+            for (int i = 0; i < seoul.Length; i++)
+            {
+                if (seoul[i] == "Kim") return "김서방은 " + i.ToString() + "에 있다";
+            }
+
+            return "";
+        }
+    }
+
+    public class A_1_15_divisor
+    {
+        public static int[] solution(int[] arr, int divisor)
+        {
+            Array.Sort(arr);
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % divisor == 0)
+                {
+                    list.Add(arr[i]);
+                }
+            }
+
+            int[] answer = new int[list.Count];
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                answer[i] = list[i];
+            }
+
+            return answer;
+        }
+    }
+
+    public class A_1_16_YinYang
+    {
+        public static int solution(int[] absolutes, bool[] signs)
+        {
+            int answer = 0;
+            for (int i = 0; i < absolutes.Length; i++)
+            {
+                answer = signs[i] ? answer + absolutes[i] : answer - absolutes[i];
+            }
+            return answer;
+        }
+    }
+    public class A_1_17_phone_number
+    {
+        public static string solution(string phone_number)
+        {
+            char[] answer = phone_number.ToCharArray();
+
+            for (int i = 0; i < answer.Length - 4; i++)
+            {
+                answer[i] = '*';
+            }
+
+            return new string(answer);
+        }
+    }
+
+    public class A_1_18_numbers
+    {
+        public int solution(int[] numbers)
+        {
+            int answer = 45;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                answer -= numbers[i];
+            }
+            
+            return answer;
+        }
+
+        public int solution2(int[] numbers)
+        {
+            var numberArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            return numberArray.Except(numbers).Sum();
+        }
+    }
+
+    public class A_1_19_Remove_smallest_number
+    {
+        public static int[] solution(int[] arr)
+        {
+            if (arr.Length < 2) return new int[] {-1};
+            return arr.Except(new int[] { arr.Min() }).ToArray();
         }
     }
 }
