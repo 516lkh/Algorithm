5ff66b11805d574e7d95c71385afab9f6e8d0476
516lkh <516rudgus@gmail.com>
2023-09-06 09:47:00 +0900

    Update Programers_Level_2.cs

--- Programers_Level_2.cs
+++ Programers_Level_2.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -73,6 +74,107 @@ namespace Algorithm
             }
 
             return count;
+        }
+    }
+    class P_2_최소직사각형
+    {
+        public int solution(int[,] sizes)
+        {
+            int w = 0, h = 0;
+
+            for(int i =0; i<sizes.GetLength(0); i++)
+            {
+                if (sizes[i, 0] < sizes[i, 1])
+                {
+                    int tmp = sizes[i, 0];
+                    sizes[i, 0] = sizes[i, 1];
+                    sizes[i, 1] = tmp;
+                }
+
+                if (w < sizes[i, 0])
+                {
+                    w = sizes[i, 0];
+                }
+
+                if (h < sizes[i, 1])
+                {
+                    h = sizes[i, 1];
+                }
+            }
+
+            return w * h;
+        }
+    }
+    class P_2_시저암호
+    {
+        public string solution(string s, int n)
+        {
+            char[] chars = s.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (n >= 26)
+                {
+                    while (n < 26)
+                    {
+                        n -= 26;
+                    }
+                }
+
+                if (chars[i] != ' ')
+                {
+                    if (chars[i] > 96)
+                    {
+                        chars[i] = (char)(chars[i] + n);
+
+                        if (chars[i] > 122)
+                        {
+                            chars[i] = (char)(chars[i] - 26);
+                        }
+                    }
+
+                    else
+                    {
+                        chars[i] = (char)(chars[i] + n);
+
+                        if (chars[i] > 90)
+                        {
+                            chars[i] = (char)(chars[i] - 26);
+                        }
+                    }
+                }
+            }
+
+            return new string(chars);
+        }
+    }
+    class P_2_문자열과_영단어
+    {
+        public int solution(string s)
+        {
+            s = s.Replace("zero", "0");
+            s = s.Replace("one", "1");
+            s = s.Replace("two", "2");
+            s = s.Replace("three", "3");
+            s = s.Replace("four", "4");
+            s = s.Replace("five", "5");
+            s = s.Replace("six", "6");
+            s = s.Replace("seven", "7");
+            s = s.Replace("eight", "8");
+            s = s.Replace("nine", "9");
+
+            return int.Parse(s);
+
+        }
+    }
+    class P_2_문자열_내마음대로_정렬하기
+    {
+        public string[] solution(string[] strings, int n)
+        {
+
+            Array.Sort(strings, (x, y) => (x.ToCharArray()[n] == y.ToCharArray()[n]) ? x.CompareTo(y) : x.ToCharArray()[n].CompareTo(y.ToCharArray()[n]));
+
+            return strings;
+
         }
     }
 }
